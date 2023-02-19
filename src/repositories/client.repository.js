de9ddcfee75c58b91f9d699b8d1087dce0e92404
madson <madsonsilva2005@gmail.com>
2023-02-19 20:53:56 -0300

    finalizado endpoint de cadastro de clientes através de um insert into no banco de dados

--- src/repositories/client.repository.js
+++ src/repositories/client.repository.js
@@ -1,9 +1,23 @@
-
+import { connect } from "./db.js";
 
 const insertClient = async (client) => {
-  
-}
+  const conn = await connect();
+
+  const sql =
+    "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+  const values = [
+    client.name,
+    client.cpf,
+    client.phone,
+    client.email,
+    client.address,
+  ];
+
+  const res = await conn.query(sql, values);
+
+  return res.rows[0];
+};
 
 export default {
-  insertClient
-}
+  insertClient,
+};
--- src/repositories/db.js
+++ src/repositories/db.js
@@ -1,18 +1,15 @@
-import pg from "pg"
+import pg from "pg";
 
-const connect = async () =>{
-  if(global.connection){
-    return global.connection.connect()
+export const connect = async () => {
+  if (global.connection) {
+    return global.connection.connect();
   }
-  
-  const pool = new pg.Pool({
-    connectionString: "postgres://dojlxqxi:RRJWt2nlNflTfISLBsHTgyfQV6MtH0Yd@babar.db.elephantsql.com/dojlxqxi"
-  })
-  global.connection = pool;
-  
-  return pool.connect();
-}
 
-export default{
-  connect
-}
+  const pool = new pg.Pool({
+    connectionString:
+      "postgres://dojlxqxi:RRJWt2nlNflTfISLBsHTgyfQV6MtH0Yd@babar.db.elephantsql.com/dojlxqxi",
+  });
+  global.connection = pool;
+
+  return pool.connect();
+};
