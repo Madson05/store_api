5de8efd49fab27c44f5f45c04d9b8c98fafffc7e
madson <madsonsilva2005@gmail.com>
2023-02-19 21:15:39 -0300

    implementada release no reposity

--- src/repositories/client.repository.js
+++ src/repositories/client.repository.js
@@ -2,20 +2,25 @@ import { connect } from "./db.js";
 
 const insertClient = async (client) => {
   const conn = await connect();
+  try {
+    const sql =
+      "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const values = [
+      client.name,
+      client.cpf,
+      client.phone,
+      client.email,
+      client.address,
+    ];
 
-  const sql =
-    "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-  const values = [
-    client.name,
-    client.cpf,
-    client.phone,
-    client.email,
-    client.address,
-  ];
+    const res = await conn.query(sql, values);
 
-  const res = await conn.query(sql, values);
-
-  return res.rows[0];
+    return res.rows[0];
+  } catch (error) {
+    throw error;
+  } finally {
+    conn.release();
+  }
 };
 
 export default {
