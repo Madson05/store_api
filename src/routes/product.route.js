c9049a9114b281d034ca87ae3e4e24f9ff102002
madson <madsonsilva2005@gmail.com>
2023-02-21 15:56:56 -0300

    implementado router do product

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,5 +1,12 @@
-import express from "express"
+import express from "express";
+import productController from "../controllers/product.controller.js";
 
 const router = express.Router();
+
+router.post("/", productController.createProduct);
+router.get("/", productController.getProducts);
+router.get("/:id", productController.getProduct);
+router.get("/delete/:id", productController.deleteProduct);
+router.put("/update", productController.updateProduct);
 
 export default router;
